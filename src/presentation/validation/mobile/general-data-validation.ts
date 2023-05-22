9f79f1ea905c28c0b27323efe8b87a0f4846291d
Annas Rusdi Awandari <w4ndaja@gmail.com>
2023-05-22 11:34:02 +0700

    remove inspection date

--- src/presentation/validation/mobile/general-data-validation.ts
+++ src/presentation/validation/mobile/general-data-validation.ts
@@ -3,8 +3,4 @@ import { z } from "zod";
 export const generalDataCreateSchema = z.object({
     customerId: z.string(),
     personInCharge: z.string(),
-    inspectionDate: z
-        .string()
-        .datetime()
-        .transform((val) => new Date(val)),
 });
